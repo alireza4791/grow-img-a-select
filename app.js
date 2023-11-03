9c1234d15038649bd0fd93de00c685fe4cb809e5
netherygo <overlord479@gmail.com>
2023-11-03 18:22:47 +0330

    .

--- app.js
+++ app.js
@@ -15,14 +15,13 @@ let servicesImgProps = {
   width: 15,
 };
 
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
+function isVisible (ele) {
+  const { top, bottom } = ele.getBoundingClientRect();
+  const vHeight = (window.innerHeight || document.documentElement.clientHeight);
+
   return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-    (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    (top > 0 || bottom > 0) &&
+    top < vHeight
   );
 }
 
