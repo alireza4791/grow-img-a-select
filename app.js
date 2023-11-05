10a4e37479c6272d2a2a8b30e708233ae2e7b2f6
alireza ghasemabady <79018776+alireza4791@users.noreply.github.com>
2023-11-05 16:14:57 +0330

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ document.addEventListener("scroll", () => {
         if (scrollYDiff === 0) {
           scrollYDiff = scrollY;
         } else {
-          servicesImgProps.width = (scrollY - scrollYDiff) / 9;
+          servicesImgProps.width = (scrollY - scrollYDiff) / 8;
           servicesHomeImgs.forEach((img) => {
             img.querySelector(".service-header").style.transitionDelay = '0.5s';
             if (!img.classList.contains("finished")) {
