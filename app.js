25f71d8db165944f672dbb92d34f6bec9c33f10e
alireza ghasemabady <79018776+alireza4791@users.noreply.github.com>
2023-11-05 15:52:15 +0330

    Update app.js

--- app.js
+++ app.js
@@ -68,13 +68,13 @@ document.addEventListener("scroll", () => {
             img.querySelector(".service-header").style.transitionDelay = '0.5s';
             if (!img.classList.contains("finished")) {
               img.style.width = `${Math.max(
-                Math.min(servicesImgProps.width, 28),
+                Math.min(servicesImgProps.width, 30),
                 15
               )}%`;
             }
           });
 
-          if (Math.max(Math.min(servicesImgProps.width, 28), 15) === 28) {
+          if (Math.max(Math.min(servicesImgProps.width, 30), 15) === 30) {
             servicesHomeImgs.forEach((img) => {
               if (!img.classList.contains("finished")) {
                 img.classList.add("finished");
@@ -156,7 +156,7 @@ servicesHomeContainer.addEventListener("mouseleave", () => {
   servicesHomeImgs.forEach((img) => {
     if (img.classList.contains("finished")) {
       img.querySelector(".service-header").style.transitionDelay = '0s';
-      img.style.width = `28%`;
+      img.style.width = `30%`;
       img.querySelector(".service-header").style.opacity = "1";
       img.querySelector(".service-header").style.transform =
         "translate(0px, 50%)rotate(-90deg)";
@@ -175,7 +175,7 @@ servicesHomeContainer.addEventListener("touchleave", () => {
   servicesHomeImgs.forEach((img) => {
     if (img.classList.contains("finished")) {
       img.querySelector(".service-header").style.transitionDelay = '0s';
-      img.style.width = `28%`;
+      img.style.width = `30%`;
       img.querySelector(".service-header").style.opacity = "1";
       img.querySelector(".service-header").style.transform =
         "translate(0px, 50%)rotate(-90deg)";
