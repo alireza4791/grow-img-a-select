2edfd8dd9076673201efd06b0ddfdd3b98002b92
alireza ghasemabady <79018776+alireza4791@users.noreply.github.com>
2024-01-06 09:19:47 +0330

    Update app.js

--- app.js
+++ app.js
@@ -407,8 +407,8 @@ const onpartnerSliderButton = (index) => {
                     if (visibleSlidesPartnersSlider + 1 === index) {
                         currentStatePartnersSlider = 1;
                     } else {
-                        console.log(Math.ceil(index / Math.floor(visibleSlidesPartnersSlider / 2)))
-                        currentStatePartnersSlider = Math.ceil(index / Math.floor(visibleSlidesPartnersSlider / 2));
+                        console.log(Math.abs(index - activeSlideIndexPartnersSlider) / 3)
+                        currentStatePartnersSlider = Math.abs(index - activeSlideIndexPartnersSlider) / 3;
                     }
                 }
 
