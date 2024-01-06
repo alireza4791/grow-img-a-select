5a22881050f0cbda548cc43f70d3aa045dfcd980
alireza ghasemabady <79018776+alireza4791@users.noreply.github.com>
2024-01-06 09:00:35 +0330

    Update app.js

--- app.js
+++ app.js
@@ -407,6 +407,7 @@ const onpartnerSliderButton = (index) => {
                     if (visibleSlidesPartnersSlider + 1 === index) {
                         currentStatePartnersSlider = 1;
                     } else {
+                        console.log(Math.floor(index / Math.floor(visibleSlidesPartnersSlider / 2)))
                         currentStatePartnersSlider = Math.floor(index / Math.floor(visibleSlidesPartnersSlider / 2));
                     }
                 }
